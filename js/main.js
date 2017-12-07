e504fc48f6a0560111c08d26471861215dca8ca2
andrey-kotsuba <kotsubaandruxa.gmail.com>
2017-12-07 21:27:14 +0200

    hover column

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 $(document).ready(function () {
 
-    $(".navigation").find(".navigation_container").mousemove(function () {
-        $(".column").removeClass("column-hover");
-        $(".column").addClass("column-not-hover");
-        $(".column:hover").removeClass("column-not-hover");
-        $(".column:hover").addClass("column-hover");
-    });
-
-    $(".navigation").find(".navigation_container").mouseleave(function () {
-        $(".column").removeClass("column-hover");
-        $(".column").removeClass("column-not-hover");
-    });
 });
