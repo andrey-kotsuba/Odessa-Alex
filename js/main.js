7f2e3e0a5788f00c25f7d8269b0cbda044d8d80d
andrey-kotsuba <kotsubaandruxa.gmail.com>
2017-12-13 02:23:23 +0200

    add slider for products

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-
+    $(".product-slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+    // $('.product-slider').slick({
+    //     infinite: true,
+    //     autoplay: true,
+    //     autoplaySpeed: 5000,
+    // })
 });
