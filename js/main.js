294b9aab93e242e038cbea79e172773f572c6981
andrey-kotsuba <kotsubaandruxa.gmail.com>
2017-12-14 00:31:24 +0200

    slider in progress

--- js/main.js
+++ js/main.js
@@ -19,4 +19,11 @@ $(document).ready(function () {
             }
         ]
     });
+
+    $(".slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
 });
