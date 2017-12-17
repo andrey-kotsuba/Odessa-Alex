ce7744ae25a9f3c9ff96e5ee1bfacd73c47dd2e1
andrey-kotsuba <kotsubaandruxa.gmail.com>
2017-12-17 20:22:46 +0200

    product-page done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+
+    $(".tabs").find("div").click(function(){
+        $(".tabs").find("div").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".description-tab").click(function(){
+        $(".containers").find("div").removeClass("active");
+        $(".description-container").addClass("active");
+    });
+
+    $(".characteristics-tab").click(function(){
+        $(".containers").find("div").removeClass("active");
+        $(".characteristics-container").addClass("active");
+    });
+
+    $(".reviews-tab").click(function(){
+        $(".containers").find("div").removeClass("active");
+        $(".reviews-container").addClass("active");
+    });
+
+    $('.big-photos').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.small-photos'
+    });
+    $('.small-photos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.big-photos',
+        focusOnSelect: true
+    });
+
     $(".product-slider").slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -50,4 +85,6 @@ $(document).ready(function () {
     });
     $( "#amountMinMax" ).val( $( "#amount" ).slider( "values", 0 ) + "л" +
         " - "  + $( "#amount" ).slider( "values", 1 ) + "л");
+
+
 });
