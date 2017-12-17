246a1e6082962a5289861502f900079959a49c88
andrey-kotsuba <kotsubaandruxa.gmail.com>
2017-12-17 17:18:02 +0200

    catalog html done

--- js/main.js
+++ js/main.js
@@ -26,4 +26,28 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 3000
     });
+
+    $( "#weight" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+            $( "#weightMinMax" ).val(ui.values[ 0 ] + "кг" + " - " + ui.values[ 1 ]+ "кг" );
+        }
+    });
+    $( "#weightMinMax" ).val( $( "#weight" ).slider( "values", 0 ) + "кг" +
+        " - "  + $( "#weight" ).slider( "values", 1 ) + "кг");
+
+    $( "#amount" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+            $( "#amountMinMax" ).val(ui.values[ 0 ] + "л" + " - " + ui.values[ 1 ]+ "л" );
+        }
+    });
+    $( "#amountMinMax" ).val( $( "#amount" ).slider( "values", 0 ) + "л" +
+        " - "  + $( "#amount" ).slider( "values", 1 ) + "л");
 });
